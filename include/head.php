0478e74e93b10f8aabfbfaf67d32871c6b63e90d
Ersin <ersintekinelb@gmail.com>
2021-12-31 05:10:22 +0300

    Update head.php

--- include/head.php
+++ include/head.php
@@ -1,4 +1,5 @@
 <?php $base_url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];?>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <title>Aurora Creation</title>
 <?php include 'include/include_style.php';?>
