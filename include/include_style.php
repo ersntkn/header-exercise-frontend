56c46fe45447a6026eabccb21b1b38c3e8a951c9
Ersin <ersintekinelb@gmail.com>
2021-12-31 05:01:16 +0300

    Update include_style.php

--- include/include_style.php
+++ include/include_style.php
@@ -2,4 +2,3 @@
 <link href="<?php echo $base_url; ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $base_url; ?>assets/fontawesome/css/all.min.css" rel="stylesheet">
 <link href="<?php echo $base_url; ?>assets/fonts/arial.ttf" rel="stylesheet" type="text/css"/>
-a
