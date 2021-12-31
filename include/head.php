0f5238d5115818477d3382265afdee72191a3e23
Ersin <ersintekinelb@gmail.com>
2021-12-31 05:34:55 +0300

    last changes
    icon colors fixed on all browsers

--- include/head.php
+++ include/head.php
@@ -1,5 +1,5 @@
 <?php $base_url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];?>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Aurora Creation</title>
 <?php include 'include/include_style.php';?>
